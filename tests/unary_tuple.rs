2474a550398c0f3034ad7ca3fd3586516037fdbb
andeya <andeyalee@outlook.com>
2024-09-15 00:13:49 +0800

    Add 'WithSetters' proc_macro_derive
    Co-Authored-By: John Baublitz <john.m.baublitz@gmail.com>

--- tests/unary_tuple.rs
+++ tests/unary_tuple.rs
@@ -1,15 +1,17 @@
-use getset::{CopyGetters, Getters, MutGetters, Setters};
+use getset::{CopyGetters, Getters, MutGetters, Setters, WithSetters};
 
 #[test]
 fn test_unary_tuple() {
-    #[derive(Setters, Getters, MutGetters)]
-    struct UnaryTuple(#[getset(set, get, get_mut)] i32);
+    #[derive(Setters, Getters, MutGetters, WithSetters)]
+    struct UnaryTuple(#[getset(set, get, get_mut, set_with)] i32);
 
     let mut tup = UnaryTuple(42);
     assert_eq!(tup.get(), &42);
     assert_eq!(tup.get_mut(), &mut 42);
     tup.set(43);
     assert_eq!(tup.get(), &43);
+    tup = tup.set_with(44);
+    assert_eq!(tup.get(), &44);
 
     #[derive(CopyGetters)]
     struct CopyUnaryTuple(#[getset(get_copy)] i32);
@@ -20,8 +22,8 @@ fn test_unary_tuple() {
 
 #[test]
 fn test_unary_tuple_with_attrs() {
-    #[derive(Setters, Getters, MutGetters)]
-    #[getset(set, get, get_mut)]
+    #[derive(Setters, Getters, MutGetters, WithSetters)]
+    #[getset(set, get, get_mut, set_with)]
     struct UnaryTuple(i32);
 
     let mut tup = UnaryTuple(42);
@@ -29,6 +31,8 @@ fn test_unary_tuple_with_attrs() {
     assert_eq!(tup.get_mut(), &mut 42);
     tup.set(43);
     assert_eq!(tup.get(), &43);
+    tup = tup.set_with(44);
+    assert_eq!(tup.get(), &44);
 
     #[derive(CopyGetters)]
     #[getset(get_copy)]
