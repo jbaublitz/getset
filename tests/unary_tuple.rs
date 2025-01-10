860de385682ac7ae447ff7d15b54e04b1ad514fa
Jason Hsieh <jason71319jason@gmail.com>
2025-01-10 16:34:06 +0800

    test: add more testcases for unary struct

--- tests/unary_tuple.rs
+++ tests/unary_tuple.rs
@@ -17,3 +17,23 @@ fn test_unary_tuple() {
     let tup = CopyUnaryTuple(42);
     assert_eq!(tup.get(), 42);
 }
+
+#[test]
+fn test_unary_tuple_with_attrs() {
+    #[derive(Setters, Getters, MutGetters)]
+    #[getset(set, get, get_mut)]
+    struct UnaryTuple(i32);
+
+    let mut tup = UnaryTuple(42);
+    assert_eq!(tup.get(), &42);
+    assert_eq!(tup.get_mut(), &mut 42);
+    tup.set(43);
+    assert_eq!(tup.get(), &43);
+
+    #[derive(CopyGetters)]
+    #[getset(get_copy)]
+    struct CopyUnaryTuple(i32);
+
+    let tup = CopyUnaryTuple(42);
+    assert_eq!(tup.get(), 42);
+}
